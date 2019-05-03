bd1c5e0bc74a26c905243e1fb067f889ad300c4d
Mark-C-uk <mark.cockcroft@gmail.com>
2019-05-03 07:14:48 +0100

    Update humidity-alert.groovy

--- humidity-alert.groovy
+++ humidity-alert.groovy
@@ -16,11 +16,12 @@
 definition(
     name: "Humidity Alert!",
     namespace: "docwisdom",
-    author: "Brian Critchlow",
+    author: "mc - Brian Critchlow",
     description: "Notify me when the humidity rises above or falls below the given threshold. It will turn on a switch when it rises above the first threshold and off when it falls below the second threshold.",
     category: "Convenience",
     iconUrl: "https://graph.api.smartthings.com/api/devices/icons/st.Weather.weather9-icn",
-    iconX2Url: "https://graph.api.smartthings.com/api/devices/icons/st.Weather.weather9-icn?displaySize=2x"
+    iconX2Url: "https://graph.api.smartthings.com/api/devices/icons/st.Weather.weather9-icn?displaySize=2x",
+    pausable: true
 )
 
 
@@ -41,61 +42,89 @@ preferences {
 	section("Control this switch:") {
 		input "switch1", "capability.switch", required: false
 	}
+    //to build over ride switch
+    //section("over ride when off:") {
+	//	input "switch2", "capability.switch", required: false
+	//}
 }
 
 def installed() {
 	subscribe(humiditySensor1, "humidity", humidityHandler)
+    subscribe(switch1, "switch", switchstate)
+    //subscribe(switch2, "switch", switchstate)
 }
 
 def updated() {
 	unsubscribe()
 	subscribe(humiditySensor1, "humidity", humidityHandler)
+    subscribe(switch1, "switch", switchstate)
+    //subscribe(switch2, "switch", switchstate)
+}
+
+def switchstate(evt){
+//if switch 2
+	state.sstate = evt.value
+log.debug "swith: ${evt.value}"
+    if (evt.value == "off" && state.appstate == "on"){
+    	log.trace "swith is '${evt.value}' , but App is still '${state.appstate}' turning on - Should be on event and app on"
+    	switch1?.on()
+    }
 }
 
 def humidityHandler(evt) {
-	log.trace "humidity: ${evt.value}"
-    log.trace "set point: ${humidity1}"
-
+log.debug "Reported humidity: ${evt.value}, set point upper: ${settings.humidity1}, set point lower: ${settings.humidity2}"
 	def currentHumidity = Double.parseDouble(evt.value.replace("%", ""))
-	def tooHumid = humidity1 
-    def notHumidEnough = humidity2
-	def mySwitch = settings.switch1
-	def deltaMinutes = 10 
-    
-    def timeAgo = new Date(now() - (1000 * 60 * deltaMinutes).toLong())
-	def recentEvents = humiditySensor1.eventsSince(timeAgo)
-	log.trace "Found ${recentEvents?.size() ?: 0} events in the last ${deltaMinutes} minutes"
-	def alreadySentSms = recentEvents.count { Double.parseDouble(it.value.replace("%", "")) >= tooHumid } > 1 || recentEvents.count { Double.parseDouble(it.value.replace("%", "")) <= notHumidEnough } > 1
-    
-	if (currentHumidity >= tooHumid) {
-		log.debug "Checking how long the humidity sensor has been reporting >= ${tooHumid}"
-
-		// Don't send a continuous stream of text messages
+//High ------------------------
+	if (currentHumidity >= settings.humidity1) {
+		if (state.sstate == "on" && state.appstate == "on"){ // everything on - Don't send a continuous stream of text messages
+        	log.trace "Humid High, Switch is ${state?.sstate}, App State is ${state.appstate} everything on already"
+        }
+		else if (state.sstate == "on" && state.appstate == "off"){ //fan on for some other reason
+        	log.trace "Humid High, Switch is ${state.sstate}, App State is ${state.appstate} switch on but app not send message anyway turning app on"
+            send("${humiditySensor1.label} sensed high humidity level of ${evt.value}")
+            state.appstate = "on"
+        }
+        else { //app off and/or switch off
+        	state.appstate = "on"
+			log.trace "Humidity High, Rose Above ${settings.humidity1}:  sending SMS and activating ${settings?.switch1} Switch is ${state.sstate}, App State is ${state.appstate}"
+			send("${humiditySensor1.label} sensed high humidity level of ${evt.value}")
+            switch1?.on()
+		}
+	log.debug "end of high hum"
+	}
+//Low -------------------
+    else if (currentHumidity <= settings.humidity2) {
+		if (state.sstate == "off" && state.appstate == "off"){
+			log.debug "low Hum Switch is ${state.sstate}, App State is ${state.appstate} no action"
+        }
 		
-
-
-		if (alreadySentSms) {
-			log.debug "Notification already sent within the last ${deltaMinutes} minutes"
-			
-		} else {
-			log.debug "Humidity Rose Above ${tooHumid}:  sending SMS to $phone1 and activating ${mySwitch}"
-			send("${humiditySensor1.label} sensed humidity rose above ${evt.value}")
-			switch1?.on()
-		}
+        else if (state.sstate == "on" && state.appstate == "on") {
+        	log.trace "Humidity Fell Below ${settings.humidity2}:  sending SMS and turning off ${settings?.switch1}"
+			state.appstate = "off"
+			send("${humiditySensor1.label} sensed LOW humidity level of ${evt.value} and de-activating ${settings?.switch1}")
+            switch1?.off()
+        }
+            
+        else if (state.sstate == "off" && state.appstate == "on"){
+        	log.trace "Humid Low, Switch is ${state.sstate}, App State is ${state.appstate} turning app off and sending off message"
+            state.appstate = "off"
+            send("${humiditySensor1.label} sensed LOW humidity level of ${evt.value}")
+        }
+        else {
+        	log.trace "Humid Low, Switch is ${state.sstate}, App State is ${state.appstate} , No action"
+        }
 	}
-
-    if (currentHumidity <= notHumidEnough) {
-		log.debug "Checking how long the humidity sensor has been reporting <= ${notHumidEnough}"
-
-		if (alreadySentSms) {
-			log.debug "Notification already sent within the last ${deltaMinutes} minutes"
-			
-		} else {
-			log.debug "Humidity Fell Below ${notHumidEnough}:  sending SMS to $phone1 and activating ${mySwitch}"
-			send("${humiditySensor1.label} sensed humidity fell below ${evt.value}")
-			switch1?.off()
-		}
+// middle --------------------------
+    else {
+    	if (state.sstate == "off" && state.appstate == "on"){
+        	log.trace "Humid Middle, ${settings.humidity1} - '${currentHumidity}' - ${settings.humidity2}, Switch is ${state.sstate}, App State is ${state.appstate} turning on again"
+        	switch1?.on()
+     	}
+        else {
+        	log.trace "Humid Middle, Switch is ${state.sstate}, App State is ${state.appstate} , No action"
+        }
 	}
+    log.debug "end of hume event"
 }
 
 private send(msg) {
